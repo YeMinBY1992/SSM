df3e09e7edf5bcd44622ecb5980f9498598a513c
zhengchaojie <398119090@qq.com>
2016-04-13 11:53:25 +0800

    change location

--- src/test/java/com/ambition/passing/test/TestMyBatis.java
+++ src/test/java/com/ambition/passing/test/TestMyBatis.java
@@ -11,7 +11,7 @@ import com.ambition.passing.pojo.User;
 import com.ambition.passing.service.IUserService;
 
 @RunWith(SpringJUnit4ClassRunner.class) // 表示继承了SpringJUnit4ClassRunner类
-@ContextConfiguration(locations = { "classpath:spring-mybatis.xml" })
+@ContextConfiguration(locations = { "classpath:spring.xml" })
 
 public class TestMyBatis {
 	private static Logger logger = Logger.getLogger(TestMyBatis.class);
